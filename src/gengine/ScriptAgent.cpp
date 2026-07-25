180a834c4f479182faa51f5d5ba31cda9d573061
Wenhuan Li <hikerhauk@gmail.com>
2026-07-24 22:37:01 -0400

    Add myself to source code copyright

--- src/gengine/ScriptAgent.cpp
+++ src/gengine/ScriptAgent.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2004 Ivo Danihelka (ivo@danihelka.net)
+ * Copyright (C) 2026 Wenhuan Li (hikerhauk@gmail.com)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
